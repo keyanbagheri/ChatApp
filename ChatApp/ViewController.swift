cd72ea47b51ec9cf7411987483a2a44094424614
keyanbagheri <keyanbagheri@gmail.com>
2017-04-07 16:19:21 +0800

    adding gitignore

--- ChatApp/ViewController.swift
+++ ChatApp/ViewController.swift
@@ -9,7 +9,12 @@
 import UIKit
 
 class ViewController: UIViewController {
+    var user : String?
 
+    @IBOutlet weak var nameTextField: UITextView!
+    @IBAction func loginButtonTapped(_ sender: Any) {
+        self.user = self.nameTextField
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
